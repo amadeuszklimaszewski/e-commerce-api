01e8943dcbec33a2ef23e85451bcfb5afbdd00d5
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-03-29 17:37:22 +0200

    First user serializer

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path, include
+from django.urls import include, re_path, path
+from rest_framework.routers import DefaultRouter
+from src.apps.accounts.viewsets import UserViewSet
 
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+
+urlpatterns = [
+    re_path(r"^", include(router.urls)),
+]
+
+urlpatterns += [path("", include("rest_framework.urls"))]
