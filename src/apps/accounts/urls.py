73df8954c47e9bd1b3a891ec05f955ac6a23f47f
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-03-30 18:28:53 +0200

    Adresses endpoint

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
-from .views import RegistrationCreateAPIView, UserProfileListAPIView
+from .views import (
+    AdressListCreateAPIView,
+    RegistrationCreateAPIView,
+    UserProfileListAPIView,
+)
 
 urlpatterns = [
     path("", include("rest_framework.urls")),
     path("users/", UserProfileListAPIView.as_view()),
     path("registration/", RegistrationCreateAPIView.as_view()),
+    path("addresses/", AdressListCreateAPIView.as_view()),
 ]
